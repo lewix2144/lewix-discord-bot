4c1ddb45503b8d3a7902397429af8881ab772a13
lewix <81369152+lewixdev@users.noreply.github.com>
2022-02-04 04:59:41 +0300

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,21 @@
+const config = require("../config.json");
+const Discord = require("discord.js");
+module.exports = {
+  name: "help", // Command Name
+  description: "List all of my commands/info about a specific command.", 
+  aliases: ["h"], // Aliases
+  usage: " ", // Usage
+  cooldown: 5, // Cooldown In Seconds
+  guildOnly: false, // If Command In DMs return
+  admin: false, // Bot Admin only
+  async execute(message, args, client) {    
+      let prefix = config.prefix
+    
+    
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`Help Menu`)
+    .setDescription(`The prefix for this server is ${prefix}`)
+    .addField(`General commands:`, `${prefix}help - Shows all the commands you can use!\n`);
+    message.channel.send(embed)
+  }
+};
